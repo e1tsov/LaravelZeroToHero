ce7fcba479b578ab9216b6820471db9d742be857
Rue-pro <ruesullivanpro@gmail.com>
2020-06-30 18:21:10 +0400

    Make ConatctController.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,4 +6,24 @@
     <h1>Контакты</h1>  
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, placeat veniam, aperiam incidunt reiciendis deserunt,
      eaque quas accusantium iusto itaque facilis sequi. Error amet eveniet debitis harum, exercitationem adipisci maxime.</p>
+     <form action="{{ route('contact-form')}}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="Введите email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Отправить</button>
+     </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
+
+Route::post('/contacts/submit', function() {
+    dd(Request::all());
+})->name('contact-form');
