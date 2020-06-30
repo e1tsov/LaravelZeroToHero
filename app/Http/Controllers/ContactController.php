648415e518234a090c553b2def551a7b73a7835a
Rue-pro <ruesullivanpro@gmail.com>
2020-06-30 18:29:24 +0400

    Add avalidation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function submit(Request $req) {
+        $validation = $req->validate([
+            'subject' => 'required|min:3|max:50'
+        ])
         dd($req->input('subject'));
     }
 }
