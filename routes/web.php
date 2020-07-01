f25bdeaaa160a44e75425b7731732e22b72c6f3c
Rue-pro <ruesullivanpro@gmail.com>
2020-07-01 18:34:34 +0400

    Print DD data in view

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
+Route::get('/contacts/all', 'ContactController@allData')->name('contact-data');
 Route::post('/contacts/submit', 'ContactController@submit')->name('contact-form');
